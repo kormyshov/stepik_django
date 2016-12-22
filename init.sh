cff7dd9917cbd45a5d1e31df6c3f96b519685da2
Mikhail Kormyshov <kormyshov@gmail.com>
2016-12-22 04:04:16 +0300

    With sudo

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
-﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-/etc/init.d/nginx restart
-ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-/etc/init.d/gunicorn restart
-﻿/etc/init.d/mysql start﻿
+sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+
