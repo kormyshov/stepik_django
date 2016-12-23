c4b1279b500f188aecce57f659fbc1d5458fadc6
Mikhail Kormyshov <kormyshov@gmail.com>
2016-12-23 03:37:28 +0300

    Fix trim

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def application(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    lst = environ['QUERY_STRING'][1:].split('&')
+    lst = environ['QUERY_STRING'].split('&')
     body = '\n'.join(lst)
     start_response(status, headers)
     return [body]
