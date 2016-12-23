8190583cbbceb1d8d873c765df389d41daff659e
Mikhail Kormyshov <kormyshov@gmail.com>
2016-12-23 03:25:29 +0300

    Task 2

---
+++ hello.py
@@ -0,0 +1,8 @@
+def wsgi_application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    lst = environ['QUERY_STRING'][1:].split('&')
+    body = '\n'.join(lst)
+    start_response(status, headers)
+    return [body]
+
