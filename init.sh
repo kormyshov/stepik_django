1e733eb234438340e589722d3740b1cfffa05c1c
Mikhail Kormyshov <kormyshov@gmail.com>
2020-04-16 00:25:21 +0300

    fix

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ﻿ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -s /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/gunicorn_django.conf
 sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start﻿
+sudo /etc/init.d/mysql start﻿
