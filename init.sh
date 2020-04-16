319b0cf537232084b538d35a7747123c37d5d83e
Mikhail Kormyshov <kormyshov@gmail.com>
2020-04-17 00:15:14 +0300

    create database

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -s /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/gunicorn_django.conf
+sudo ln -sf /home/box/web/etc/gunicorn_django.conf /etc/gunicorn.d/gunicorn_django.conf
 sudo /etc/init.d/gunicorn restart
-sudo /etc/init.d/mysql start﻿
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+~/web/ask/manage.py makemigrations
+~/web/ask/manage.py migrate
